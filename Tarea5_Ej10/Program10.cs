89afa14322d4fa1cece957098e46e3643e875199
smbarrionuevo2024 <169066260+smbarrionuevo2024@users.noreply.github.com>
2024-06-09 15:12:59 -0300

    Add files via upload

---
+++ Tarea5_Ej10/Program10.cs
@@ -0,0 +1,34 @@
+﻿/* 10. Crear una función a la que se le pasa un número entero y devuelve la cantidad de 
+ * divisores primos que tiene. */
+
+using System;
+
+namespace Tarea5_Ej10
+{
+    internal class Program10
+    {
+        static void Main(string[] args)
+        {
+            // Declaro las Variables
+            int nro;
+
+            // Le pido al usuario que ingrese un nro
+            Console.WriteLine("Ingrese un Número: ");
+            nro = int.Parse(Console.ReadLine());
+
+            // Llamo a la función CantDivisores
+            CantDivisores(nro);
+        }
+
+        static void CantDivisores(int numero)
+        {
+            for (int i = 1; i <= 9; i++)
+            {
+                if(numero %i == 0)
+                {
+                    Console.WriteLine("\nEl nro " + numero + " es divisible por " + i);
+                }
+            }
+        }
+    }
+}
---
+++ Tarea5_Ej9/Program9.cs
@@ -0,0 +1,46 @@
+﻿/* 8. Se necesita una función que calcule y muestre en pantalla el área o el volumen de un cilindro, 
+ * según se especifique. Para distinguir un caso de otro, además de pasarle por parámetro el radio y 
+ * la altura, se le pasará el carácter ’a’ (para área) o ’v’ (para el volumen).
+ * 
+ * 9. Idem al anterior pero que devuelva un array con ambos cálculos: el área y el volumen.  */
+
+using System;
+
+namespace Tarea5_Ej9
+{
+    internal class Program9
+    {
+        static void Main(string[] args)
+        {
+            // Realizo las variables del Area y la Altura
+            double radio, altura;
+
+            Console.Write("Ingrese el valor del Radio del Cilindro: ");
+            radio = double.Parse(Console.ReadLine());
+
+            Console.Write("\nIngrese el valor de la Altura del Cilindro: ");
+            altura = double.Parse(Console.ReadLine());
+
+            PaseParametros(radio, altura);
+        }
+
+        static void PaseParametros(double rad, double alt)
+        {
+            // Realizo las variables
+            double radio, altura;
+
+            // Cargo el valor de area( radio * radio * PI )
+            double[] area = { rad * rad * Math.PI };
+
+
+            Console.WriteLine("\nEl Area del Circulo es: " + area[0]);
+
+
+            // Cargo el valor del volumen( Pi * radio * radio * altura)
+            double[] volumen = { Math.PI * rad * rad * alt };
+
+            Console.WriteLine("\nEl Volumen del circuito es: " + volumen[0] + " cm3");
+            Console.ReadLine();
+        }
+    }
+}
